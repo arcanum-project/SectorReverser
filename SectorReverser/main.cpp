182dbcff5014567e5bc3d5a8e1533ff08bd08fd7
Dima <belousovdmn@gmail.com>
2022-08-08 12:05:17 -0700

    Initial Commit

---
+++ SectorReverser/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SectorReverser
+//
+//  Created by Dmitrii Belousov on 8/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
